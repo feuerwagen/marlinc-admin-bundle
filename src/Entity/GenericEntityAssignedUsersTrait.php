a1744115b2824461d43c136e3323672b2b9f17e7
Elias Müller <em@aha.biz>
2018-06-14 13:04:49 +0200

    Added trait for user assignment checks on generic referencing entities.

---
+++ src/Entity/GenericEntityAssignedUsersTrait.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: em
+ * Date: 14.06.18
+ * Time: 13:01
+ */
+
+namespace Marlinc\AdminBundle\Entity;
+
+
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+trait GenericEntityAssignedUsersTrait
+{
+    /**
+     * @inheritDoc
+     */
+    public static function getParents(): array
+    {
+        return []; // No dedicated field for parent entities.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUsers(): Collection
+    {
+        // Nothing to do here, files don't have own users.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setUsers(Collection $users)
+    {
+        // Nothing to do here, files don't have own users.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addUser(UserInterface $user)
+    {
+        // Nothing to do here, files don't have own users.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function removeUser(UserInterface $user)
+    {
+        // Nothing to do here, files don't have own users.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasUser(UserInterface $user): bool
+    {
+        if ($this->getReferencingEntities()->isEmpty()) {
+            return true;
+        }
+
+        foreach ($this->getReferencingEntities() as $entity) {
+            if ($entity instanceof EntityAssignedUsersInterface && $entity->hasUser($user)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
