26dddd490157016b0515cb0f5662cc942879ba9c
majid <majid@keen-on.de>
2021-03-29 14:47:40 +0200

    Updated : services.xml and EntityRouterBuilder.php

--- src/Route/EntityRouterBuilder.php
+++ src/Route/EntityRouterBuilder.php
@@ -2,29 +2,42 @@
 
 namespace Marlinc\AdminBundle\Route;
 
+use Sonata\AdminBundle\Model\AuditManagerInterface;
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Builder\RouteBuilderInterface;
-use Sonata\AdminBundle\Model\AuditManagerInterface;
-use Sonata\AdminBundle\Route\PathInfoBuilder;
+use Sonata\AdminBundle\Route\RouteCollection;
 
-class EntityRouterBuilder implements RouteBuilderInterface
+class EntityRouterBuilder  implements RouteBuilderInterface
 {
-    /**
-     * @var AuditManagerInterface
-     */
+    private $decorated;
     private $manager;
 
-    public function __construct(AuditManagerInterface $manager)
+    /**
+     * @var TrashManagerInterface
+     */
+    protected $trashManager;
+
+    /**
+     * @param \Sonata\AdminBundle\Model\AuditManagerInterface $manager
+     * @param TrashManagerInterface $trashManager
+     */
+    public function __construct(PathInfoBuilder $decorated,AuditManagerInterface $manager)
     {
+        $this->decorated = $decorated;
+
         $this->manager = $manager;
     }
-
-    public function build(AdminInterface $admin, RouteCollectionInterface $collection,PathInfoBuilder $pathInfoBuilder): void
+    /**
+     * @param \Sonata\AdminBundle\Admin\AdminInterface $admin
+     * @param \Sonata\AdminBundle\Route\RouteCollection $collection
+     */
+    public function build(AdminInterface $admin, RouteCollection $collection)
     {
-        $pathInfoBuilder->build($admin, $collection);
+        $this->decorated->build($admin, $collection);
 
         if ($this->manager->hasReader($admin->getClass())) {
             $collection->add('history_revert', $admin->getRouterIdParameter() . '/history/{revision}/revert');
         }
+
     }
 }
