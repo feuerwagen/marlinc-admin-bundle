88f6bf152356bedc4072e964d0fcc49f4a2549c4
Elias Müller <em@aha.biz>
2021-08-01 23:46:57 +0200

    New export transformer needed a little fix.

--- src/Transformer/PostfixTransformer.php
+++ src/Transformer/PostfixTransformer.php
@@ -18,6 +18,10 @@ class PostfixTransformer implements TransformerInterface
      */
     public function transform(string $name, int $type, array $data)
     {
-        return [$name => $data.$this->postfix];
+        foreach ($data as $key => $value) {
+            $data[$key] = $value.$this->postfix;
+        }
+
+        return [$name => implode('', $data)];
     }
 }
